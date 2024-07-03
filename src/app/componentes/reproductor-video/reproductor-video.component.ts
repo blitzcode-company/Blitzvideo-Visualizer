749f02dd9747c59ded6bfa7715597c3cf457784d
Mateo Sosa <mateesosar@gmail.com>
2024-07-03 17:59:01 -0300

    Diseño para reproductor de video

--- src/app/componentes/reproductor-video/reproductor-video.component.ts
+++ src/app/componentes/reproductor-video/reproductor-video.component.ts
@@ -10,6 +10,8 @@ export class ReproductorVideoComponent implements OnInit, AfterViewInit {
   @Input() videoUrl: string | undefined;
 
   @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef<HTMLVideoElement> | undefined;
+  @ViewChild('progressBar', { static: true }) progressBar!: ElementRef<HTMLInputElement>;
+  @ViewChild('volumeSlider', { static: true }) volumeSlider!: ElementRef<HTMLInputElement>;
 
   constructor(private el: ElementRef) { }
 
@@ -18,6 +20,69 @@ export class ReproductorVideoComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     if (this.videoPlayer?.nativeElement) {
       this.videoPlayer.nativeElement.load();
+    }
+  }
+
+
+  isPlaying = false;
+  isMuted = false;
+
+ 
+  togglePlayPause() {
+    const video: HTMLVideoElement | undefined = this.videoPlayer?.nativeElement;
+    if (video) {
+      if (video.paused || video.ended) {
+        video.play();
+        this.isPlaying = true;
+      } else {
+        video.pause();
+        this.isPlaying = false;
+      }
+    }
+  }
+
+  updateProgressBar() {
+    const video: HTMLVideoElement | undefined = this.videoPlayer?.nativeElement;
+    const progressBar: HTMLInputElement | undefined = this.progressBar?.nativeElement;
+    if (video && progressBar) {
+      const progress = (video.currentTime / video.duration) * 100;
+      progressBar.value = progress.toString();
+    }
+  }
+
+  seek(progressBar: HTMLInputElement) {
+    const video: HTMLVideoElement | undefined = this.videoPlayer?.nativeElement;
+    if (video) {
+      const time = (parseInt(progressBar.value) / 100) * video.duration;
+      video.currentTime = time;
+    }
+  }
+
+  toggleMute() {
+    const video: HTMLVideoElement | undefined = this.videoPlayer?.nativeElement;
+    if (video) {
+      video.muted = !video.muted;
+      this.isMuted = video.muted;
+    }
+  }
+
+  changeVolume(volumeSlider: HTMLInputElement) {
+    const video: HTMLVideoElement | undefined = this.videoPlayer?.nativeElement;
+    if (video) {
+      video.volume = parseInt(volumeSlider.value) / 100;
+    }
+  }
+
+  toggleFullscreen() {
+    const video: HTMLVideoElement | undefined = this.videoPlayer?.nativeElement;
+    if (video) {
+      if (!document.fullscreenElement) {
+        video.requestFullscreen().catch(err => {
+          alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+        });
+      } else {
+        document.exitFullscreen();
+      }
     }
   }
 }
